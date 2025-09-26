33a36b567749aeff8b8f63d17e2540090c1ca8dc
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-09-26 10:30:48 +0300

    Update urls.py

--- carts/urls.py
+++ carts/urls.py
@@ -3,6 +3,6 @@ from .views import CartDetailView, AddToCartView, RemoveFromCartView
 
 urlpatterns = [
     path("cart/", CartDetailView.as_view(), name="cart-detail"),
-    path("cart/add/", AddToCartView.as_view(), name="add-to-cart"),  # API DRF
-    path("cart/remove/<int:product_id>/", RemoveFromCartView.as_view(), name="remove-from-cart"),  # API DRF
+    path("cart/add/", AddToCartView.as_view(), name="add-to-cart"),  
+    path("cart/remove/<int:product_id>/", RemoveFromCartView.as_view(), name="remove-from-cart"),  
 ]
